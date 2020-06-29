9adc94806e3d6e272d52a2ced288be1b47fb02f5
morgan <morgan.b.chen@gmail.com>
2020-06-29 14:49:31 +0800

    初始化项目

---
+++ src/main/java/com/morgan/hongfu/HongFuApplication.java
@@ -0,0 +1,18 @@
+package com.morgan.hongfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 启动类
+ * @Author Morgan
+ * @Date 2020/6/29 14:45
+ **/
+@SpringBootApplication
+public class HongFuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongFuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morgan/hongfu/HongFuApplicationTests.java
@@ -0,0 +1,11 @@
+package com.morgan.hongfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class HongFuApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
